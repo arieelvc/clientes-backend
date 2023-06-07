f69a89b7ff9beaf3487b9db5ad27f8b88d55f310
Ariel Castan <arielpvc17@gmail.com>
2023-06-07 01:02:31 -0400

    app_clientes_back_end

---
+++ spring-boot-backend-apirest/src/main/java/com/ariel/spring/boot/backend/apirest/models/dao/IClienteDao.java
@@ -0,0 +1,17 @@
+package com.ariel.spring.boot.backend.apirest.models.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+
+import com.ariel.spring.boot.backend.apirest.models.entity.Cliente;
+import com.ariel.spring.boot.backend.apirest.models.entity.Region;
+
+
+public interface IClienteDao extends JpaRepository<Cliente, Long>{
+
+	@Query("from Region")
+	public List<Region> findAllRegiones();
+}
---
+++ spring-boot-backend-apirest/src/main/java/com/ariel/spring/boot/backend/apirest/models/services/ClienteServiceImpl.java
@@ -0,0 +1,58 @@
+package com.ariel.spring.boot.backend.apirest.models.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ariel.spring.boot.backend.apirest.models.dao.IClienteDao;
+import com.ariel.spring.boot.backend.apirest.models.entity.Cliente;
+import com.ariel.spring.boot.backend.apirest.models.entity.Region;
+
+
+@Service
+public class ClienteServiceImpl implements IClienteService{
+
+	@Autowired
+	private IClienteDao clienteDao;
+	
+	@Override
+	@Transactional(readOnly = true) // los metodos transactional ya vienen con esto, no es necesario agregarlo
+	public List<Cliente> findAll() {
+
+		return (List<Cliente>) clienteDao.findAll();
+	}
+	@Override
+	@Transactional(readOnly = true)
+	public Page<Cliente> findAll(Pageable pageable) {
+		return clienteDao.findAll(pageable);
+	}
+	
+	@Override
+	@Transactional(readOnly = true)
+	public Cliente findById(Long id) {
+		return clienteDao.findById(id).orElse(null);
+	}
+
+	@Override
+	@Transactional
+	public Cliente save(Cliente cliente) {
+		return clienteDao.save(cliente);
+	}
+
+	@Override
+	@Transactional
+	public void delete(Long id) {
+		clienteDao.deleteById(id);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<Region> findAllRegiones() {
+		return clienteDao.findAllRegiones();
+	}
+
+}
